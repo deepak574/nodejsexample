98ad90702cecbbacf60648346c8b50e81cd638fe
Deepak <gujjuladeepak>
2024-10-20 06:21:20 +0530

    small changes

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 app.get('/',(req,res) => {
 	res.send("Hello World, I am from Docker");
 });
-app.listen(3040,() => {
-	console.log("Server is running on port 3040")
+app.listen(3020,() => {
+	console.log("Server is running on port 3020")
 });
