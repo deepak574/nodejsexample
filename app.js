dd1d9e0e02c47c2c281f52afc2d54401a33bf658
Deepak <gujjuladeepak>
2024-10-20 06:31:48 +0530

    changed the git content

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res) => {
-	res.send("Hello World, I am from Docker");
+	res.send("Hello World, I am from Nodejs");
 });
 app.listen(3020,() => {
 	console.log("Server is running on port 3020")
